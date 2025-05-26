363dc33f9d64373d6d81f5915fc239e864922022
nkowalczuk4 <ayumii69@icloud.com>
2025-05-26 22:29:47 +0200

    tetast

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,9 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Kotlin"
-    val test = "test1234"
+    val test = "test123423123"
     println("Hello, " + name + "!")
+    println(test)
 
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
